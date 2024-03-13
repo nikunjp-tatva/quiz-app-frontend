0d6f6aaf43a9a3eb2c33cd4b732426139909579e
nikunjp-tatva <nikunj.polara@tatvasoft.com>
2024-03-13 19:27:49 +0530

    login form validation

--- src/pages/Auth/form/LoginForm.tsx
+++ src/pages/Auth/form/LoginForm.tsx
@@ -1,64 +1,105 @@
+import React, { useCallback } from "react";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useForm } from "react-hook-form";
+import * as Yup from 'yup';
 
 import { FormInputText } from "../../../common/form-component/FormInputText";
 
 interface IFormInput {
-  email: string;
-  password: string;
+    email: string;
+    password: string;
 }
 
 const defaultValues = {
-  email: "",
-  password: "",
+    email: "",
+    password: "",
 };
 
+const validationSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Email is required'),
+    password: Yup.string().required('Password is required').min(4),
+});
+
+const useYupValidationResolver = (validationSchema: Yup.ObjectSchema<{ email: string; password: string; }, Yup.AnyObject, { email: undefined; password: undefined; }, "">) =>
+    useCallback(
+        async (data: any) => {
+            try {
+                const values = await validationSchema.validate(data, {
+                    abortEarly: false,
+                })
+
+                return {
+                    values,
+                    errors: {},
+                }
+            } catch (errors: any) {
+                return {
+                    values: {},
+                    errors: errors.inner.reduce(
+                        (allErrors: any, currentError: { path: any; type: any; message: any; }) => ({
+                            ...allErrors,
+                            [currentError.path]: {
+                                type: currentError.type ?? "validation",
+                                message: currentError.message,
+                            },
+                        }),
+                        {}
+                    ),
+                }
+            }
+        },
+        [validationSchema]
+    )
+
 const LoginForm = () => {
-  const { handleSubmit, reset, control, setValue, watch } = useForm<IFormInput>(
-    {
-      defaultValues: defaultValues,
-    },
-  );
+    const resolver = useYupValidationResolver(validationSchema)
 
-  const onSubmit = (data: IFormInput) => console.log(data);
+    const { handleSubmit, reset, control, setValue, watch } = useForm<IFormInput>(
+        {
+            defaultValues,
+            resolver,
+        },
+    );
 
-  return (
-    <Box>
-      <Stack mb={3}>
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="email"
-          mb="5px"
-          mt="25px"
-        >
-          Email Address
-        </Typography>
-        <FormInputText name="email" control={control} type="email" />
+    const onSubmit = (data: IFormInput) => console.log(data);
 
-        <Typography
-          variant="subtitle1"
-          fontWeight={600}
-          component="label"
-          htmlFor="password"
-          mb="5px"
-          mt="25px"
-        >
-          Password
-        </Typography>
-        <FormInputText name="password" control={control} type="password" />
-      </Stack>
-      <Button
-        onClick={handleSubmit(onSubmit)}
-        variant="contained"
-        size="small"
-        fullWidth
-      >
-        Sign In
-      </Button>
-    </Box>
-  );
+    return (
+        <Box>
+            <Stack mb={3}>
+                <Typography
+                    variant="subtitle1"
+                    fontWeight={600}
+                    component="label"
+                    htmlFor="email"
+                    mb="5px"
+                    mt="25px"
+                >
+                    Email Address
+                </Typography>
+                <FormInputText name="email" control={control} type="email" />
+
+                <Typography
+                    variant="subtitle1"
+                    fontWeight={600}
+                    component="label"
+                    htmlFor="password"
+                    mb="5px"
+                    mt="25px"
+                >
+                    Password
+                </Typography>
+                <FormInputText name="password" control={control} type="password" />
+            </Stack>
+            <Button
+                onClick={handleSubmit(onSubmit)}
+                variant="contained"
+                size="small"
+                fullWidth
+            >
+                Sign In
+            </Button>
+        </Box>
+    );
 };
 
 export default LoginForm;
